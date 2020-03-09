f880233d40f037ba8f5aea53ae0753857be9c494
dimovich85 <dimovich85@gmail.com>
2020-03-09 20:20:55 +0200

    Урок 8

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+add_action('after_setup_theme', 'si_setup');
+
+add_filter('show_admin_bar', '__return_false');
+
+function si_setup(){
+    add_theme_support( 'custom-logo' );
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'post-thumbnails' );
+}
+
+function _si_assets_path( $path ){
+    return get_template_directory_uri() . '/assets/' . $path;
+}
