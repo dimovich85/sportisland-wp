9d0a3928fe03b982a708dc32e925308c87a3deb8
dimovich85 <dimovich85@gmail.com>
2020-03-16 15:02:24 +0200

    Урок 11

--- functions.php
+++ functions.php
@@ -6,22 +6,26 @@ add_action('wp_enqueue_scripts', 'si_scripts');
 add_filter('show_admin_bar', '__return_false');
 
 function si_setup(){
+    register_nav_menu( 'menu-header', 'Меню в шапке' );
+    register_nav_menu( 'menu-footer', 'Меню в подвале' );
+    
     add_theme_support( 'custom-logo' );
     add_theme_support( 'title-tag' );
     add_theme_support( 'post-thumbnails' );
+    //add_theme_support( 'menus' );
 }
 
 function si_scripts(){
     wp_enqueue_script(
         'js',
-        get_template_directory_uri() . '/assets/js/js.js',
+        _si_assets_path('js/js.js'),
         [],
         '1.0',
         true
     );
     wp_enqueue_style(
         'si-style',
-        get_template_directory_uri() . '/assets/css/styles.css',
+        _si_assets_path('css/styles.css'),
         [],
         '1.0',
         'all'
