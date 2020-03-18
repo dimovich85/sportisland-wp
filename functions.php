7f5a5cd08f08d57b47e4706cc48b49cef14889d2
dimovich85 <dimovich85@gmail.com>
2020-03-18 14:13:44 +0200

    Урок 14

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once(__DIR__ . '/inc/widget-text.php');
+
 add_action('after_setup_theme', 'si_setup');
 add_action('wp_enqueue_scripts', 'si_scripts');
 add_action( 'widgets_init', 'si_register' );
@@ -76,6 +78,8 @@ function si_register(){
         'before_widget' => null,
         'after_widget' => null
     ]);
+
+    register_widget( 'si_widget_text' );
 }
 
 function _si_assets_path( $path ){
