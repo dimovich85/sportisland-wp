72d0988c94c3b63d1def8fa669e2aa25bb98e2c6
dimovich85 <dimovich85@gmail.com>
2020-03-21 18:37:57 +0200

    Урок 19

--- functions.php
+++ functions.php
@@ -17,10 +17,13 @@ add_action('after_setup_theme', 'si_setup');
 add_action('wp_enqueue_scripts', 'si_scripts');
 add_action( 'widgets_init', 'si_register' );
 add_action( 'init', 'si_register_types' );
+add_action( 'add_meta_boxes', 'si_meta_boxes' );
+add_action( 'save_post',  'si_save_like_meta' );
 add_shortcode( 'si-paste-link', 'si_paste_link' );
 
 add_filter('show_admin_bar', '__return_false');
 add_filter('si_widget_text', 'do_shortcode');
+
 
 function si_setup(){
     register_nav_menu( 'menu-header', 'Меню в шапке' );
@@ -280,6 +283,28 @@ function si_paste_link( $attr ){
         return "<a href=\"${link}\">${text}</a>";
     } else {
         return '';
+    }
+}
+
+function si_meta_boxes(){
+    add_meta_box(
+        'si-like',
+        'Количество лайков: ',
+        'si_meta_like_cb',
+        'post'
+    );
+}
+
+function si_meta_like_cb( $post_obj ){
+    $likes = get_post_meta( $post_obj->ID, 'si-like',  true);
+    $likes = $likes ? $likes : 0;
+    echo "<input type=\"text\" name=\"si-like\" value=\"${likes}\">";
+    //echo '<p>' . $likes . '</p>';
+}
+
+function si_save_like_meta( $post_id ){
+    if( isset( $_POST['si-like'] ) ){
+        update_post_meta( $post_id, 'si-like', $_POST['si-like'] );
     }
 }
 
