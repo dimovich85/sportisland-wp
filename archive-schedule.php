2e75c635b17a0f6b00ef8540f839078c712ed8ef
dimovich85 <dimovich85@gmail.com>
2020-04-01 14:49:52 +0300

    Показать страницу

--- archive-schedule.php
+++ archive-schedule.php
@@ -8,33 +8,65 @@
         <h1 class="main-heading schedule-page__h">расписание</h1>
         <div class="tabs">
           <ul class="tabs-btns">
-            <li class="tabs-btns__item active-tab">
-              <a href="#mon" class="tabs-btns__btn"> Пн </a>
+          <?php
+            $days = get_terms([
+              'taxonomy' => 'schedule_days',
+              'order' => 'ASC',
+              'orderby' => 'slug'
+            ]);
+            $index = 0;
+            $active_class = '';
+            foreach( $days as $day ):
+              if( $index === 0 ){
+                $active_class = ' active-tab';
+              } else {
+                $active_class = '';
+              }
+          ?>
+            <li class="tabs-btns__item<?php echo $active_class; ?>">
+              <a 
+                href="#<?php echo $day->slug; ?>" 
+                class="tabs-btns__btn"
+                aria-label="<?php echo $day->description; ?>"
+              >
+                <?php echo $day->name; ?>
+              </a>
             </li>
-            <li class="tabs-btns__item">
-              <a href="#tue" class="tabs-btns__btn"> Вт </a>
-            </li>
-            <li class="tabs-btns__item">
-              <a href="#wed" class="tabs-btns__btn"> Ср </a>
-            </li>
-            <li class="tabs-btns__item">
-              <a href="#thur" class="tabs-btns__btn"> Чт </a>
-            </li>
-            <li class="tabs-btns__item">
-              <a href="#fri" class="tabs-btns__btn"> Пт </a>
-            </li>
-            <li class="tabs-btns__item">
-              <a href="#sat" class="tabs-btns__btn"> Сб </a>
-            </li>
-            <li class="tabs-btns__item">
-              <a href="#sun" class="tabs-btns__btn"> Вс </a>
-            </li>
+          <?php
+            $index++;
+            endforeach;
+          ?>
           </ul>
           <ul class="tabs-content">
-            <li class="tabs-content__item active-tab" id="mon">
-              <h2 class="sr-only"> Понедельник </h2>
+          <?php
+            $index = 0;
+            foreach( $days as $day ):
+              if( $index === 0 ){
+                $active_class = 'active-tab';
+              } else{
+                $active_class = '';
+              }
+          ?>
+            <li 
+              class="tabs-content__item<?php echo $active_class; ?>" 
+              id="<?php echo $day->slug; ?>"
+            >
+              <h2 class="sr-only"><?php echo $day->description; ?></h2>
               <ul class="schedule tabs-content__list">
+              <?php
+                $actions = [];
+                foreach( $actions as $act ):
+              ?>
                 <li class="schedule__item">
+                  <p class="schedule__time"> 07:00 - 22:00 </p>
+                  <h2 class="schedule__h"> Фитнесс </h2>
+                  <p class="schedule__trainer"> с Литвиненко Ольгой </p>
+                  <p class="schedule__place"> фитнесс зал </p>
+                </li>
+              <?
+                endforeach;
+              ?>
+              <li class="schedule__item">
                   <p class="schedule__time"> 07:00 - 22:00 </p>
                   <h2 class="schedule__h"> Фитнесс </h2>
                   <p class="schedule__trainer"> с Литвиненко Ольгой </p>
@@ -84,59 +116,10 @@
                 </li>
               </ul>
             </li>
-            <li class="tabs-content__item" id="tue">
-              <h2 class="sr-only"> Вторник </h2>
-              <ul class="schedule tabs-content__list">
-                <li class="schedule__item">
-                  <p class="schedule__time"> 10:00 - 22:00 </p>
-                  <h2 class="schedule__h"> Фитнесс ВТОРНИК </h2>
-                  <p class="schedule__trainer"> с Литвиненко Ольгой </p>
-                  <p class="schedule__place"> фитнесс зал </p>
-                </li>
-                <li class="schedule__item">
-                  <p class="schedule__time"> 07:00 - 22:00 </p>
-                  <h2 class="schedule__h"> Фитнесс </h2>
-                  <p class="schedule__trainer"> с Литвиненко Ольгой </p>
-                  <p class="schedule__place"> фитнесс зал </p>
-                </li>
-                <li class="schedule__item">
-                  <p class="schedule__time"> 07:00 - 22:00 </p>
-                  <h2 class="schedule__h"> Фитнесс </h2>
-                  <p class="schedule__trainer"> с Литвиненко Ольгой </p>
-                  <p class="schedule__place"> фитнесс зал </p>
-                </li>
-                <li class="schedule__item">
-                  <p class="schedule__time"> 07:00 - 22:00 </p>
-                  <h2 class="schedule__h"> Фитнесс </h2>
-                  <p class="schedule__trainer"> с Литвиненко Ольгой </p>
-                  <p class="schedule__place"> фитнесс зал </p>
-                </li>
-                <li class="schedule__item">
-                  <p class="schedule__time"> 07:00 - 22:00 </p>
-                  <h2 class="schedule__h"> Фитнесс </h2>
-                  <p class="schedule__trainer"> с Литвиненко Ольгой </p>
-                  <p class="schedule__place"> фитнесс зал </p>
-                </li>
-                <li class="schedule__item">
-                  <p class="schedule__time"> 07:00 - 22:00 </p>
-                  <h2 class="schedule__h"> Фитнесс </h2>
-                  <p class="schedule__trainer"> с Литвиненко Ольгой </p>
-                  <p class="schedule__place"> фитнесс зал </p>
-                </li>
-                <li class="schedule__item">
-                  <p class="schedule__time"> 07:00 - 22:00 </p>
-                  <h2 class="schedule__h"> Фитнесс </h2>
-                  <p class="schedule__trainer"> с Литвиненко Ольгой </p>
-                  <p class="schedule__place"> фитнесс зал </p>
-                </li>
-                <li class="schedule__item">
-                  <p class="schedule__time"> 07:00 - 22:00 </p>
-                  <h2 class="schedule__h"> Фитнесс </h2>
-                  <p class="schedule__trainer"> с Литвиненко Ольгой </p>
-                  <p class="schedule__place"> фитнесс зал </p>
-                </li>
-              </ul>
-            </li>
+          <?php
+            $index++;
+            endforeach;
+          ?>
           </ul>
         </div>
       </div>
