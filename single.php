3f7fe5daf5001d8daf523e2f53f77653696847c9
dimovich85 <dimovich85@gmail.com>
2020-04-03 16:57:58 +0300

    Урок 33б

--- single.php
+++ single.php
@@ -13,7 +13,16 @@
       ?>
       <article class="main-article wrapper">
         <header class="main-article__header">
-          <?php the_post_thumbnail('full', ['class'=> 'main-article__thumb']); ?>
+          <?php
+            $custom_thumb = get_field('post_si_thumb');
+            if( $custom_thumb ){
+              $url = $custom_thumb['url'];
+              $alt = $custom_thumb['alt'];
+              echo "<img src=\"$url\" alt=\"$alt\" class=\"main-article__thumb\">";
+            } else{
+              the_post_thumbnail('full', ['class'=> 'main-article__thumb']);
+            }
+          ?>
           <h1 class="main-article__h">
             <?php the_title(); ?>
           </h1>
